80caa42b51799445e5133730b745389b7a5cdc00
RupertBarrow <rupert.barrow@lilo.org>
2021-12-17 00:02:08 +0100

    Appel du lanceur Heroku depuis Salesforce

--- src/server/processes/web.ts
+++ src/server/processes/web.ts
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://enterprise-platform-112-dev-ed.lightning.force.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
