1fd2484fc4ee3ffb1ff5bebe4f977b7fe4236fdf
RupertBarrow <rupert.barrow@lilo.org>
2021-12-16 20:36:51 +0100

    Appel du lanceur Heroku depuis Salesforce

--- src/server/processes/web.ts
+++ src/server/processes/web.ts
@@ -34,10 +34,10 @@ app.use(cors());
 
 // RBW - Open CORS for all origins
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
